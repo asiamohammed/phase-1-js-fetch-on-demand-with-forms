6bd6819bfc1e1c19f4619764744b6e3a8f5f678c
asiamohammed <mohamedasia021@gmail.com>
2022-09-09 06:20:20 +0300

    completed assignement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const init = () => {
-  
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const id = document.getElementById('searchByID').value;
+        fetchMovies(id);
+    });
+
+}
+async function fetchMovies(id) {
+    // To pass the tests, don't forget to return your fetch!
+    const response = await fetch(`http://localhost:3000/movies/${id}`);
+    const data = await response.json();
+    return render(data);
+
+}
+
+function render(data) {
+    const parent = document.querySelector("ul");
+    const list = document.createElement("li");
+    const title = document.createElement("h3");
+    title.innerText = data.title;
+    const id = document.createElement("div");
+    id.innerText = `ID: ${data.id}`;
+
+    list.appendChild(title);
+    list.appendChild(id);
+    parent.appendChild(list);
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
